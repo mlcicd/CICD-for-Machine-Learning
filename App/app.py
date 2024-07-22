f974cb21ceb2a97d165421f9a2349519934bce77
mlcicd <38316127+mlcicd@users.noreply.github.com>
2024-07-22 16:08:05 +0200

    fix untrusted types

--- App/app.py
+++ App/app.py
@@ -1,19 +1,20 @@
 import gradio as gr
 import skops.io as sio
 
-pipe = sio.load("./Model/drug_pipeline.skops")
+# Get the untrusted types
+untrusted_types = sio.get_untrusted_types(file="./Model/drug_pipeline.skops")
+
+pipe = sio.load("./Model/drug_pipeline.skops", trusted=untrusted_types)
 
 
 def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
     """Predict drugs based on patient features.
-
     Args:
         age (int): Age of patient
         sex (str): Sex of patient
         blood_pressure (str): Blood pressure level
         cholesterol (str): Cholesterol level
         na_to_k_ratio (float): Ratio of sodium to potassium in blood
-
     Returns:
         str: Predicted drug label
     """
@@ -22,7 +23,6 @@ def predict_drug(age, sex, blood_pressure, cholesterol, na_to_k_ratio):
 
     label = f"Predicted Drug: {predicted_drug}"
     return label
-
 
 inputs = [
     gr.Slider(15, 74, step=1, label="Age"),
@@ -39,11 +39,9 @@ examples = [
     [50, "M", "HIGH", "HIGH", 34],
 ]
 
-
 title = "Drug Classification"
 description = "Enter the details to correctly identify Drug type?"
 article = "This app is a part of the **[Beginner's Guide to CI/CD for Machine Learning](https://www.datacamp.com/tutorial/ci-cd-for-machine-learning)**. It teaches how to automate training, evaluation, and deployment of models to Hugging Face using GitHub Actions."
-
 
 gr.Interface(
     fn=predict_drug,
